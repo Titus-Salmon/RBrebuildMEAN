651c8d064c4baeb4df9131ce5b9bb4df9d1b835a
tsalm0n <tsalm0n@twc.com>
2019-08-17 02:49:27 -0400

    1st commit - getting header resolved

---
+++ fr0nt3nd/src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule, NO_ERRORS_SCHEMA /*tod*/ } from '@angular/core';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    HeaderComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    MDBBootstrapModule.forRoot() // t0d
+  ],
+  schemas: [NO_ERRORS_SCHEMA], // t0d
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
